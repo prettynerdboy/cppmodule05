a43d64b37f5a6208ecbca6a2334e35f04e230bb7
prettynerdboy <terjehakonsen0625@gmail.com>
2025-04-06 00:28:43 +0900

    create Bureaucrat class

---
+++ ex02/Bureaucrat.cpp
@@ -0,0 +1,98 @@
+#include "Bureaucrat.hpp"
+#include "AForm.hpp"
+
+//constructor
+Bureaucrat::Bureaucrat(std::string name, int grade):_name(name)
+{
+    if(grade > 150)
+        throw (GradeTooLowException());
+    if(grade < 1)
+        throw  (GradeTooHighException());
+    _grade = grade;
+}
+//destructor
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Bureaucrat " << this->_name << " fall down" << std::endl;
+}
+
+//copy constructor
+Bureaucrat::Bureaucrat(Bureaucrat const &other)
+{
+    this->_grade=other.getGrade();
+}
+
+//copy assignment
+Bureaucrat &Bureaucrat::operator=(Bureaucrat const &other)
+{
+    if(this != &other)
+    {
+        this->_grade=other.getGrade();
+    }
+    return (*this);
+}
+
+// grade operator
+void Bureaucrat::incrementGrade()
+{
+    if(this->_grade + 1 > 150)
+        throw (GradeTooLowException());
+    this->_grade++;
+}
+
+void Bureaucrat::decrementGrade()
+{
+    if(this->_grade - 1 < 1)
+        throw (GradeTooHighException());
+    this->_grade--;
+}
+
+//accessor
+const std::string &Bureaucrat::getName() const
+{
+    return (this->_name);
+}
+
+int Bureaucrat::getGrade() const
+{
+    return (this->_grade);
+}
+
+//form acition 
+void Bureaucrat::signForm(AForm &form)
+{
+
+	try
+	{
+		form.beSigned(*this);
+		std::cout << _name << " signed " << form.getName() << std::endl;
+	}
+	catch (const std::exception &e)
+	{
+
+		std::cout << _name << " couldn’t sign " << form.getName() << " because " << e.what() << std::endl;
+	}
+}
+
+void Bureaucrat::executeForm(AForm const &form)
+{
+    try
+    {
+        form.execute(*this);
+        std::cout << _name << "executed " << form.getName() << std::endl;
+    }
+    catch(const std::exception& e)
+    {
+        std::cout << _name << "couldn't execute execute " << form.getName() << " because " << e.what() << std::endl;
+    }
+    
+}
+// ポリモーフィズムのメリットわかりやすい。
+
+std::ostream &operator<<(std::ostream &o, Bureaucrat const &bureaucrat)
+{
+
+	o << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return o;
+}
+
---
+++ ex02/Bureaucrat.hpp
@@ -0,0 +1,55 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <exception>
+
+class AForm;
+
+class Bureaucrat
+{
+    private:
+        const std::string _name;
+        int               _grade;
+    public:
+        //constructor
+        Bureaucrat(std::string name="dafault" ,int grade=1);
+        // destructor
+        ~Bureaucrat();
+        //copy constructor
+        Bureaucrat(Bureaucrat const &other);
+        //copy assignment
+        Bureaucrat &operator=(Bureaucrat const &other);
+        //grade operator
+        void incrementGrade();
+        void decrementGrade();
+        //accessor
+        const std::string &getName() const;
+        int               getGrade() const;
+        // form action
+        void signForm(AForm &form);
+        void executeForm(AForm const &form);
+        //exception class
+        class GradeTooHighException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw()
+                {
+                    return ("Bureaucrat : Grade is too high!");
+                }
+        };
+        class GradeTooLowException : public std::exception
+        {
+            public:
+                virtual const char *what() const throw()
+                {
+                    return ("Bureaucrat : Grade is too low!");
+                }
+        };
+};
+
+std::ostream &operator<<(std::ostream &os , Bureaucrat const &bureaucrat);
+
+#endif
+//constructorをプライベートメンバにすることで意図的に使わせない実装もできる。
